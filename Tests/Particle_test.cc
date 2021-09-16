e20a17c350ca49f4469f08dbc0ad5c80229c3a43
David Nathan Brown <dave_brown@lbl.gov>
2021-09-16 10:22:25 -0700

    Working PTraj: not yet cylinder display

--- Tests/Particle_test.cc
+++ Tests/Particle_test.cc
@@ -4,7 +4,7 @@
 #include "KinKal/General/AxialBFieldMap.hh"
 #include "KinKal/General/ParticleState.hh"
 #include "KinKal/Trajectory/LoopHelix.hh"
-#include "KinKal/Trajectory/PiecewiseTrajectory.hh"
+#include "KinKal/Trajectory/ParticleTrajectory.hh"
 #include "TFile.h"
 #include "TSystem.h"
 #include "TDirectory.h"
@@ -16,6 +16,13 @@
 #include "TStyle.h"
 #include "TLegend.h"
 #include "TGraph.h"
+#include "TAxis3D.h"
+#include "TPolyLine3D.h"
+#include "TPolyMarker3D.h"
+#include "TTUBE.h"
+#include "TBRIK.h"
+#include "TNode.h"
+#include "TGeometry.h"
 #include <stdio.h>
 #include <getopt.h>
 #include <iostream>
@@ -26,36 +33,46 @@
 using namespace std;
 
 void print_usage() {
-  printf("Usage: ParticleTest --pfile s --bfile s --nsample i\n");
+  printf("Usage: ParticleTest --pfile s --bfile s --zmax f --tol f --npts i\n");
 }
 
 int main(int argc, char **argv) {
-  size_t nsample(100000);
+  using KTRAJ=KinKal::LoopHelix;
+  using PKTRAJ = KinKal::ParticleTrajectory<KTRAJ>;
+  size_t npts(1000);
   string pfile, bfile;
+  double zmax(-3500), tol(1e-2);
 
   static struct option long_options[] = {
     {"pfile",     required_argument, 0, 'f' },
     {"bfile",     required_argument, 0, 'F' },
-    {"nsample",     required_argument, 0, 'N'  },
+    {"tol",     required_argument, 0, 't' },
+    {"zmax",     required_argument, 0, 'z'  },
+    {"npts",     required_argument, 0, 'N'  },
     {NULL, 0,0,0}
   };
   int opt;
   int long_index =0;
   while ((opt = getopt_long_only(argc, argv,"",
-	  long_options, &long_index )) != -1) {
+          long_options, &long_index )) != -1) {
     switch (opt) {
       case 'f' : pfile = string(optarg);
-		 break;
+                 break;
       case 'F' : bfile = string(optarg);
-		 break;
-      case 'N' : nsample = atoi(optarg);
-		 break;
+                 break;
+      case 't' : tol = atof(optarg);
+                 break;
+      case 'z' : zmax = atof(optarg);
+                 break;
+      case 'N' : npts = atoi(optarg);
+                 break;
       default: print_usage();
-	       exit(EXIT_FAILURE);
+               exit(EXIT_FAILURE);
     }
   }
-// not sure why this is necessary...
+  // not sure why this is necessary...
   gSystem->Load("lib/libTests.dylib");
+  gSystem->Load("libGeom");
   if(pfile.size()==0){
     cout << "No input pfile specified: terminating" << endl;
     return 1;
@@ -72,20 +89,87 @@ int main(int argc, char **argv) {
   std::string sourcedir = getenv("TRACKTOY_SOURCE_DIR");
   std::string fullfile = sourcedir+"/"+bfile;
   KinKal::AxialBFieldMap axfield(fullfile);
+  cout << "axial field between " << axfield.zMin() << " and " << axfield.zMax() << " with " << axfield.field().size()
+    << " field values from "  << axfield.field().front() << " to "  << axfield.field().back() << endl;
+  TFile ptestpfile("ParticleTest.root","RECREATE");
+  std::vector<TPolyLine3D*> plhel;
+  int icolor(kBlue);
+  zmax = std::min(zmax,axfield.zMax());
   while (reader.Next()) {
     cout << "pstate status " <<  pstate.GetSetupStatus() << endl;
     cout << "Read particle with position " << pstate->position3() << " time " << pstate->time() << " momentum " << pstate->momentum3() << endl;
+    cout << "pVz = " << pstate->velocity().Z() << " tmax " << (zmax-pstate->position3().Z())/pstate->velocity().Z() << endl;
 
+    KinKal::TimeRange range(pstate->time(),pstate->time()+(zmax-pstate->position3().Z())/pstate->velocity().Z());
+    cout << " initial range " << range << endl;
     // create a loop helix from this
     auto bstart = axfield.fieldVect(pstate->position3());
-    KinKal::LoopHelix lhelix(*pstate,bstart);
-    cout << "Loop Helix " << lhelix << endl;
-
+    KTRAJ lhelix(*pstate,bstart,range);
+    cout << "Initial trajectory " << lhelix << endl;
+    // initialize piecetraj
+    PKTRAJ phelix(lhelix);
+    auto pos = pstate->position3();
+    // extend to the end of the BField range
+    while(pos.Z() < zmax){
+      range.begin() = axfield.rangeInTolerance(phelix.back(),range.begin(),tol);
+      if(range.begin() < range.end()){
+        // Predict new position and momentum at this end, making linear correction for BField effects
+        auto endstate = phelix.back().state(range.begin());
+        pos = endstate.position3();
+        auto bend = axfield.fieldVect(pos);
+        KTRAJ endhelix(endstate,bend,range);
+        phelix.append(endhelix);
+        //      cout << "appended helix at point " << pos << " time " << range.begin() << endl;
+      } else
+        break;
+    }
+    double largest, average;
+    size_t igap;
+    phelix.gaps(largest, igap, average);
+    cout << "Particle piece traj with " << phelix.pieces().size() << " pieces and largest gap = "
+      << largest << " average gap = " << average << endl;
+    plhel.push_back(new TPolyLine3D(npts));
+    plhel.back()->SetLineColor(icolor++);
+    double tstart = phelix.range().begin();
+    double ts = phelix.range().range()/(npts-1);
+    KinKal::VEC3 ppos;
+    for(unsigned ipt=0;ipt<npts;ipt++){
+      double t = tstart + ipt*ts;
+      ppos = phelix.position3(t);
+      plhel.back()->SetPoint(ipt,ppos.X(),ppos.Y(),ppos.Z());
+    }
   }
-  // setup histogram
-  TFile ptestpfile("ParticleTest.root","RECREATE");
+// now draw
+  TCanvas* ptcan = new TCanvas("ptcan","ParticleTraj",1000,1000);
+//  TBRIK* cent = new TBRIK();
+//  TNode* cnode = new TNode();
+//  cnode->cd();
+  // Draw target
+//  TTUBE* target= new TTUBE("target","target","Aluminum",21.5,75.0,800.0);
+//  target->SetNumberOfDivisions(100);
+//  TNode* tnode = new TNode("targetnode","targetnode",target,0.0,0.0,-4300);
+//  cout << "targetnode " << cnode->GetZ() << endl;
+//  cout << "targetnode " << tnode->GetZ() << endl;
+//  cnode->Draw("gl");
+//  cnode->Draw();
+//  pnode->Draw();
+  for(auto const& ph : plhel) {
+    ph->Draw();
+//    plhel.front()->Draw();
+  }
+  // draw the origin and axes
+  TAxis3D* rulers = new TAxis3D();
+  rulers->GetXaxis()->SetAxisColor(kBlue);
+  rulers->GetXaxis()->SetLabelColor(kBlue);
+  rulers->GetYaxis()->SetAxisColor(kCyan);
+  rulers->GetYaxis()->SetLabelColor(kCyan);
+  rulers->GetZaxis()->SetAxisColor(kOrange);
+  rulers->GetZaxis()->SetLabelColor(kOrange);
+  rulers->GetZaxis()->SetRange(axfield.zMin(),zmax);
+  rulers->Draw();
 
-  //  diocan->Write();
+
+  ptcan->Write();
   ptestpfile.Write();
   ptestpfile.Close();
   ppfile->Close();
