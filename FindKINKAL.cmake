e8839460693cd14caecbca0bef8259fe3e73413e
David Nathan Brown <dave_brown@lbl.gov>
2021-10-12 12:09:14 -0700

    Don't assume TrackToy and KinKal have the same root directory

--- FindKINKAL.cmake
+++ FindKINKAL.cmake
@@ -1,8 +1,13 @@
 #- Try to find the KinKal package
 # Once done this will define
 #  KINKAL_FOUND - System has KinKal
+#  KINKAL_INCLUDE_DIR - The directory needed to compile against KinKal classes
 #  KINKAL_LIBRARIES - The libraries needed to use KinKal
 
+
+MESSAGE(STATUS "Adding KinKal include path $ENV{KINKAL_SOURCE_DIR}")
+set(KINKAL_INCLUDE_DIR $ENV{KINKAL_SOURCE_DIR}/..)
+mark_as_advanced(KINKAL_INCLUDE_DIR)
 MESSAGE(STATUS "Looking for KinKal libraries in directory $ENV{KINKAL_LIBRARY_DIR}")
 foreach( _cpt General MatEnv Trajectory)
   MESSAGE("Looking for Kinkal library ${_cpt}")
@@ -18,6 +23,7 @@ IF(KINKAL_LIBRARIES)
    SET(KINKAL_FOUND TRUE)
    MESSAGE(STATUS "Found KinKal libraries: ${KINKAL_LIBRARIES}")
 ENDIF()
+
 
 mark_as_advanced(KINKAL_LIBRARIES)
 
